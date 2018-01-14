c7bc8920799bf918ac89a6c3d990320499396ff6
RebertMatheusTeixeira <rebertmatheus.t@gmail.com>
2018-01-14 11:30:22 -0200

    Initial Commit with Udacity's template.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class BooksApp extends React.Component {
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
@@ -201,4 +202,5 @@ class BooksApp extends React.Component {
     )
   }
 }
+
 export default BooksApp
