8235f4e4e90c6f577a3f0c2feddb831794966ed7
RebertMatheusTeixeira <rebertmatheus.t@gmail.com>
2018-01-15 00:25:18 -0200

    Select error

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,20 +3,15 @@ import React, { Component } from 'react';
 class Book extends Component {
 
     state = {
-        UpdateShelf: 'XXX'
+        shelfUpdate: ''
     }
 
-    updateShelf(book, shelf) {
-        this.setState({UpdateShelf: shelf})
-
-        this.props.onUpdateBook(book)
-    }
     componentDidMount() {
-        this.setState({shelf: this.props.book.shelf})
+        this.setState({shelfUpdate: this.props.book.shelf})
     }
     render(){
         const {book, changeShelf, onUpdateBook} = this.props
-        const {UpdateShelf} = this.state
+        let {shelfUpdate} = this.state
         
         return(
             <div className="book">
@@ -24,7 +19,7 @@ class Book extends Component {
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                     {changeShelf && 
                         <div className="book-shelf-changer">
-                            <select id='selectShelf' onChange={() => onUpdateBook(book,() => {this.setState({UpdateShelf: 'AQUI' })})}>
+                            <select onChange={() => onUpdateBook(book,shelfUpdate)}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
